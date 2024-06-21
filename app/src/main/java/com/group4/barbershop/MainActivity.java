3456fcd70810ad148d8e99fde9d4a728a33aeeed
panipujayanti <panipujayanti@gmail.com>
2024-06-21 20:01:21 +0700

    add feature home

--- app/src/main/java/com/group4/barbershop/MainActivity.java
+++ app/src/main/java/com/group4/barbershop/MainActivity.java
@@ -1,24 +1,26 @@
 package com.group4.barbershop;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.group4.barbershop.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setupBottomNav();
+    }
+
+    private void setupBottomNav() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }
